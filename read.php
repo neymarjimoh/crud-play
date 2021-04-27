2d34472777cfc6ee9ac9e5cb5d21b1d3981fd1d9
neymarjimoh <jemohkunle2007@gmail.com>
2021-04-27 12:41:56 +0100

    fixing minor auth

--- read.php
+++ read.php
@@ -1,4 +1,13 @@
 <?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+
 // Check existence of id parameter before processing further
 if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
     // Include config file
--- update.php
+++ update.php
@@ -1,4 +1,13 @@
 <?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+
 require_once "connection.php";
  
 $title = $course_code = "";
